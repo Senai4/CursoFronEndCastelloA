13192a5c056291569093f97abc23436dc2e7bddb
senaisp5@gmail.com <senaisp5@gmail.com>
2025-03-12 11:31:44 -0300

    12.03.25

---
+++ ArrayMatrix/SA01.js
@@ -0,0 +1,46 @@
+// situação de aprendizagem 01 - Array e Matrix
+//Calculadora de Média
+
+const prompt = require("prompt-sync")();
+var notas = []; //declaração de um vetor
+
+//função para adicionar nota -> notas
+function adicionarNotas(){
+    let nota = Number(prompt("Informe a Nota:"))
+    notas.push(nota);
+    console.log("Nota Adicionda com Sucesso");
+}
+
+//função para calcular a média
+function calcularMedia(){
+    if(notas.length === 0) return;
+    let soma = notas.reduce((x,y) => x+y);
+    let mediaFinal = (soma / notas.length);
+    console.log("A média é"+mediaFinal);
+}
+
+//menu de opções 
+function menuOpcoes(){
+    let continuar = true;
+    while(continuar){
+    console.log("===Calculadora Média===");
+    console.log("|1. Adicionar Nota     |");
+    console.log("|2. Calcular Média     |");
+    console.log("|3. Sair               |");
+    console.log("=======================");
+    let operacao = prompt("Informa a Opção Desejada");
+    switch(operacao){
+        case "1": adicionarNotas;
+        break;
+        case "2": calcularMedia;
+        break;
+        case "3": continuar = false;
+        break;
+        default: console.log("Opção Inválida");
+        break;
+    }//fim do switch
+}//fim do while
+console.log("Saindo...");
+}//fim da function
+
+menuOpcoes();
